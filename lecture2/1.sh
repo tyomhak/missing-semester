0572b09fa800939d6657d5864f46edb02ebaedeb
Artyom <artyom@Artyoms-MacBook-Pro.local>
2021-01-12 20:49:19 +0400

    Solution of Ex. 1 for lecture 2

---
+++ lecture2/1.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# ls    ->  list files
+# 
+#   flags
+# l -> long format (include more data)
+# a -> include hidden files
+# h -> human readable file sizes
+# t -> time sorted (by recency)
+# G -> colorized format
+
+ls -la -h -t  -G
